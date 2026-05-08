bdcd75364d8119dfd360773cbeb092e24323ce70
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-05-09 03:01:59 +0400

    Bar Chart added for visualized mock user-agent data.

--- controllers/analyticsControllers.js
+++ controllers/analyticsControllers.js
@@ -1,10 +1,17 @@
+const path = require('path');
 const { countUserAgents } = require("../utils/mockLogger");
 
 const getUserAgentCount = (req, res) => {
     const userAgentCounts = countUserAgents();
     res.status(200).json(userAgentCounts);
+};
+
+
+const getVisualizedCharts = (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "../analytics.html"));
 }
 
 module.exports = {
-    getUserAgentCount
+    getUserAgentCount,
+    getVisualizedCharts
 };
--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const { countUserAgents } = require('../utils/mockLogger');
-const { getUserAgentCount } = require('../controllers/analyticsControllers');
+const { getUserAgentCount, getVisualizedCharts } = require('../controllers/analyticsControllers');
 const router = express.Router();
 
+
+// VISUALIZATION ROUTES
+router.get("/", getVisualizedCharts);
+
+// GET USER AGENT COUNT
 router.get("/user-agent", getUserAgentCount);
 
 module.exports = router;
